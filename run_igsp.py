8b4c21900684a461146ad294ca7464bb6b512619
Ashka Shah <shahashka11@gmail.com>
2023-01-30 15:58:37 -0600

    update run script

--- run_igsp.py
+++ run_igsp.py
@@ -4,12 +4,15 @@ import pandas as pd
 import numpy as np
 
 def run_regulon_db():
-    data = pd.read_csv("./regulondb/data_smaller.csv")
-    targets = pd.read_csv("./regulondb/targets.csv")
-    targets_index = pd.read_csv("./regulondb/targets.csv")
-
-    obs_data_no_targets = data.iloc[targets_index==1].to_numpy()
-    iv_samples_list = [data.iloc[targets_index==ind] for ind in np.arange(2, targets.shape[0]+1)]
+    data = pd.read_csv("./regulondb/data_smaller.csv", header=None)
+    targets = pd.read_csv("./regulondb/targets.csv", header=None)
+    targets_index = pd.read_csv("./regulondb/target_index.csv", header=None)
+    obs_inds = targets_index.index[targets_index.iloc[:,0]==1]
+    obs_data_no_targets = data.iloc[obs_inds].to_numpy()
+    
+    iv_samples_list = [data.iloc[targets_index.index[targets_index.iloc[:,0]==ind]].to_numpy() for ind in np.arange(1, targets.shape[0]+1)]
+    for s in iv_samples_list:
+        print(s.shape)
 
     targets_list = []
     for i in np.arange(targets.shape[0]):
@@ -17,20 +20,22 @@ def run_regulon_db():
     nodes=np.arange(1,data.shape[1]+1)
 
     setting_list = [dict(known_interventions=[t]) if type(t) !=list else dict(known_interventions=t) for t in targets_list]
-
+    print(obs_data_no_targets.shape, len(targets_list), len(nodes), len(setting_list), len(iv_samples_list))
     obs_suffstat = conditional_independence.partial_correlation_suffstat(obs_data_no_targets)
     invariance_suffstat = conditional_independence.gauss_invariance_suffstat(obs_data_no_targets, iv_samples_list)
+    #print(obs_suffstat)
+    print(len(invariance_suffstat['contexts']))
     alpha = 1e-3
     alpha_inv = 1e-3
     ci_tester = conditional_independence.MemoizedCI_Tester(conditional_independence.partial_correlation_test,
                                                            obs_suffstat, alpha=alpha)
-    invariance_tester = conditional_independence.MemoizedInvarianceTester(conditional_independence.gauss_invariance_test,
+    invariance_tester = conditional_independence.MemoizedInvarianceTester(conditional_independence.kci_invariance_test,
                                                                           invariance_suffstat, alpha=alpha_inv)
 
     est_dag, _ = unknown_target_igsp(setting_list, nodes, ci_tester, invariance_tester)
     est_adj = edge_to_adj(est_dag.arcs, list(est_dag.nodes))
     np.savetxt("./regulondb/igsp_adj.csv", est_adj, delimeter=",")
-
+    print("saved regulondb")
 def run_dream4():
     data = pd.read_csv("./insilico_size10_3/ insilico_size10_3_combine.csv")
     obs_data = data.iloc[data['target']==0]

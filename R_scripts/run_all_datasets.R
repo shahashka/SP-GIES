fd6f0675769f9490411befccc3923b40a808b56f
Ashka Shah <shahashka11@gmail.com>
2023-02-07 10:20:01 -0600

    comment out except regdb

--- R_scripts/run_all_datasets.R
+++ R_scripts/run_all_datasets.R
@@ -11,86 +11,85 @@ dataset_path <- file.path("../regulondb/data_smaller.csv", fsep=.Platform$file.s
 target_path <- file.path("../regulondb/targets.csv", fsep=.Platform$file.sep)
 target_index_path <- file.path("../regulondb/target_index.csv", fsep=.Platform$file.sep)
 
-run_from_file_sp_gies(dataset_path, target_path, target_index_path, threshold=6.917,
-skeleton_path="../regulondb/adj_mat.csv", save_path="../regulondb/clr_skel_", save_pc=TRUE)
+run_from_file_sp_gies(dataset_path, target_path, target_index_path, threshold=6.917, skeleton_path="../regulondb/adj_mat.csv", save_path="../regulondb/clr_skel_", save_pc=TRUE)
 run_from_file_gies(dataset_path, target_path, target_index_path, save_path="../regulondb/md_100_", max_degree=100)
 run_from_file_igsp(dataset_path, target_path, target_index_path, save_path="../regulondb")
 
-
-# Run DREAM4 insilico network #3
-dataset_OI_path <- file.path(paste("../insilico_size10_3/insilico_size10_3_combine.csv", sep=""), fsep=.Platform$file.sep)
-dataset <- read.table(dataset_OI_path, sep=",", header=TRUE)
-targets.index <- dataset[,ncol(dataset)]
-targets <- as.matrix(unique(targets.index))
-targets <- targets[targets > 0]
-targets_init <- list(integer(0))
-targets <- append(targets_init, targets)
-dataset <- dataset[,1:ncol(dataset)-1]
-targets.index <- targets.index + 1
-iv_data = dataset[targets.index>1]
-
-# Use skeleton from CLR as input to sp-gies algorithm
-skeleton <- read.table("../insilico_size10_3/adj_mat.csv", sep=",", header=FALSE)
-skeleton <- as(skeleton,"matrix")
-#make symmetric
-skeleton[lower.tri(skeleton)] = t(skeleton)[lower.tri(skeleton)]
-skeleton <- as.data.frame(skeleton)
-skeleton <- skeleton == 0
-class(skeleton) <- "logical"
-sp_gies_from_skeleton(dataset, targets, targets.index, skeleton, save_path="../insilico_size10_3/")
-gies(dataset, targets, targets.index, save_path="../insilico_size10_3/")
-
-dataset_O_path <- file.path(paste("../insilico_size10_3/insilico_size10_3_obs.csv", sep=""), fsep=.Platform$file.sep)
-dataset <- read.table(dataset_O_path, sep=",", header=TRUE)
-targets.index <- dataset[,ncol(dataset)]
-targets <- as.matrix(unique(targets.index))
-targets <- targets[targets > 0]
-targets_init <- list(integer(0))
-targets <- append(targets_init, targets)
-dataset <- dataset[,1:ncol(dataset)-1]
-targets.index <- targets.index + 1
-sp_gies(dataset, targets, targets.index, save_path="../insilico_size10_3/obs_", save_pc=TRUE)
-gies(dataset, targets, targets.index, save_path="../insilico_size10_3/obs_")
-
-obs_data=dataset
-run_igsp_obs_iv(obs_data, iv_data, save_path="../insilico_size10_3/")
-run_igsp_obs_iv(obs_data, iv_data, save_path="../insilico_size10_3/obs_", obs_only=TRUE)
-
-
-# Run random networks small world size 10
-num_nodes=10
-num_graphs=30
-for (network in list('ER','scale','small')) {
-    for (x in 0:29) {
-    	print(x)
-        folder <- paste("../random_test_set_",num_nodes, "_", network, "/",sep="")
-	dataset_OI_path <- paste(folder,"data_joint_", x,".csv", sep="")
-        dataset <- read.table(dataset_OI_path, sep=",", header=TRUE)
-        targets.index <- dataset[,ncol(dataset)]
-        targets <- as.matrix(unique(targets.index))
-        targets <- targets[targets > 0]
-        targets_init <- list(integer(0))
-        targets <- append(targets_init, targets)
-        dataset <- dataset[,1:ncol(dataset)-1]
-        targets.index <- targets.index + 1
-        sp_gies(dataset, targets, targets.index, save_path=paste(folder, x, "_", sep=""))
-        gies(dataset, targets, targets.index, save_path=paste(folder, x, "_", sep=""))
-        iv_data = dataset[targets.index>1]
-
-        dataset_O_path <- paste(folder, "data_", x, ".csv", sep="")
-        dataset <- read.table(dataset_O_path, sep=",", header=TRUE)
-        targets.index <- dataset[,ncol(dataset)]
-        targets <- as.matrix(unique(targets.index))
-        targets <- targets[targets > 0]
-        targets_init <- list(integer(0))
-        targets <- append(targets_init, targets)
-        dataset <- dataset[,1:ncol(dataset)-1]
-        targets.index <- targets.index + 1
-	    sp_gies(dataset, targets, targets.index, save_path=paste(folder, "/obs_", x, "_",sep=""), save_pc=TRUE)
-        gies(dataset, targets, targets.index, save_path=paste(folder, "/obs_", x, "_",sep=""))
-
-        obs_data=dataset
-        igsp(obs_data, iv_data, save_path="../insilico_size10_3/")
-        igsp(obs_data, iv_data, save_path="../insilico_size10_3/obs_", obs_only=TRUE)
-    }
-}
+#
+# # Run DREAM4 insilico network #3
+# dataset_OI_path <- file.path(paste("../insilico_size10_3/insilico_size10_3_combine.csv", sep=""), fsep=.Platform$file.sep)
+# dataset <- read.table(dataset_OI_path, sep=",", header=TRUE)
+# targets.index <- dataset[,ncol(dataset)]
+# targets <- as.matrix(unique(targets.index))
+# targets <- targets[targets > 0]
+# targets_init <- list(integer(0))
+# targets <- append(targets_init, targets)
+# dataset <- dataset[,1:ncol(dataset)-1]
+# targets.index <- targets.index + 1
+# iv_data = dataset[targets.index>1]
+#
+# # Use skeleton from CLR as input to sp-gies algorithm
+# skeleton <- read.table("../insilico_size10_3/adj_mat.csv", sep=",", header=FALSE)
+# skeleton <- as(skeleton,"matrix")
+# #make symmetric
+# skeleton[lower.tri(skeleton)] = t(skeleton)[lower.tri(skeleton)]
+# skeleton <- as.data.frame(skeleton)
+# skeleton <- skeleton == 0
+# class(skeleton) <- "logical"
+# sp_gies_from_skeleton(dataset, targets, targets.index, skeleton, save_path="../insilico_size10_3/")
+# gies(dataset, targets, targets.index, save_path="../insilico_size10_3/")
+#
+# dataset_O_path <- file.path(paste("../insilico_size10_3/insilico_size10_3_obs.csv", sep=""), fsep=.Platform$file.sep)
+# dataset <- read.table(dataset_O_path, sep=",", header=TRUE)
+# targets.index <- dataset[,ncol(dataset)]
+# targets <- as.matrix(unique(targets.index))
+# targets <- targets[targets > 0]
+# targets_init <- list(integer(0))
+# targets <- append(targets_init, targets)
+# dataset <- dataset[,1:ncol(dataset)-1]
+# targets.index <- targets.index + 1
+# sp_gies(dataset, targets, targets.index, save_path="../insilico_size10_3/obs_", save_pc=TRUE)
+# gies(dataset, targets, targets.index, save_path="../insilico_size10_3/obs_")
+#
+# obs_data=dataset
+# run_igsp_obs_iv(obs_data, iv_data, save_path="../insilico_size10_3/")
+# run_igsp_obs_iv(obs_data, iv_data, save_path="../insilico_size10_3/obs_", obs_only=TRUE)
+#
+#
+# # Run random networks small world size 10
+# num_nodes=10
+# num_graphs=30
+# for (network in list('ER','scale','small')) {
+#     for (x in 0:29) {
+#     	print(x)
+#         folder <- paste("../random_test_set_",num_nodes, "_", network, "/",sep="")
+# 	dataset_OI_path <- paste(folder,"data_joint_", x,".csv", sep="")
+#         dataset <- read.table(dataset_OI_path, sep=",", header=TRUE)
+#         targets.index <- dataset[,ncol(dataset)]
+#         targets <- as.matrix(unique(targets.index))
+#         targets <- targets[targets > 0]
+#         targets_init <- list(integer(0))
+#         targets <- append(targets_init, targets)
+#         dataset <- dataset[,1:ncol(dataset)-1]
+#         targets.index <- targets.index + 1
+#         sp_gies(dataset, targets, targets.index, save_path=paste(folder, x, "_", sep=""))
+#         gies(dataset, targets, targets.index, save_path=paste(folder, x, "_", sep=""))
+#         iv_data = dataset[targets.index>1]
+#
+#         dataset_O_path <- paste(folder, "data_", x, ".csv", sep="")
+#         dataset <- read.table(dataset_O_path, sep=",", header=TRUE)
+#         targets.index <- dataset[,ncol(dataset)]
+#         targets <- as.matrix(unique(targets.index))
+#         targets <- targets[targets > 0]
+#         targets_init <- list(integer(0))
+#         targets <- append(targets_init, targets)
+#         dataset <- dataset[,1:ncol(dataset)-1]
+#         targets.index <- targets.index + 1
+# 	    sp_gies(dataset, targets, targets.index, save_path=paste(folder, "/obs_", x, "_",sep=""), save_pc=TRUE)
+#         gies(dataset, targets, targets.index, save_path=paste(folder, "/obs_", x, "_",sep=""))
+#
+#         obs_data=dataset
+#         igsp(obs_data, iv_data, save_path="../insilico_size10_3/")
+#         igsp(obs_data, iv_data, save_path="../insilico_size10_3/obs_", obs_only=TRUE)
+#     }
+# }

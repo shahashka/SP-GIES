24e2d435f31981ccf30d61666973092a06398453
Ashka Shah <shahashka11@gmail.com>
2023-02-17 13:32:49 -0600

    fix random large run for igsp

--- run_igsp.py
+++ run_igsp.py
@@ -18,11 +18,8 @@ def run_regulon_db():
     targets_index = pd.read_csv("./regulondb/target_index.csv", header=None)
     obs_inds = targets_index.index[targets_index.iloc[:,0]==1]
     obs_data_no_targets = data.iloc[obs_inds].to_numpy()
-    print(obs_data_no_targets)
-    
+
     iv_samples_list = [data.iloc[targets_index.index[targets_index.iloc[:,0]==ind]].to_numpy() for ind in np.arange(1, targets.shape[0]+1)]
-    for s in iv_samples_list:
-        print(s.shape)
 
     targets_list = [targets.iloc[i].dropna().tolist() for i in np.arange(targets.shape[0])]
 
@@ -41,6 +38,7 @@ def run_regulon_db():
     invariance_tester = conditional_independence.MemoizedInvarianceTester(conditional_independence.gauss_invariance_test,
                                                                           invariance_suffstat, alpha=alpha_inv)
 
+    print('start learner')
     est_dag = igsp(setting_list, nodes, ci_tester, invariance_tester)
     est_dag_obs = gsp(nodes,ci_tester)
 
@@ -81,26 +79,29 @@ def run_random():
     for network in ["ER", "scale", "small"]:
         for i in range(30):
             data = pd.read_csv("./random_test_set_10_{}/data_joint_{}.csv".format(network, i))
-            obs_data = data.iloc[data['target'] == 0]
-            int_data = data.iloc[data['target'] != 0]
-            obs_data_no_targets = obs_data.loc[:, obs_data.columns != 'target'].to_numpy()
-            int_data_no_targets = int_data.loc[:, int_data.columns != 'target']
-            iv_samples_list = [np.expand_dims(row.to_numpy(), axis=0) for _, row in int_data_no_targets.iterrows()]
+            obs_data = data.loc[data['target'] == 0]
+            int_data = data.loc[data['target'] != 0]
+            targets_list = set(list(int_data['target'].to_numpy()))
+
+            obs_data = obs_data.loc[:, obs_data.columns != 'target'].to_numpy()
+
+            iv_samples_list = [int_data.loc[int_data['target'] == t] for t in targets_list]
+            iv_samples_list = [inter.loc[:, inter.columns != 'target'] for inter in iv_samples_list]
 
             targets_list = set(list(int_data['target'].to_numpy()))
             nodes = targets_list
 
             setting_list = [dict(interventions=[t]) for t in targets_list]
 
-            obs_suffstat = conditional_independence.partial_correlation_suffstat(obs_data_no_targets)
-            invariance_suffstat = conditional_independence.gauss_invariance_suffstat(obs_data_no_targets,
+            obs_suffstat = conditional_independence.partial_correlation_suffstat(obs_data)
+            invariance_suffstat = conditional_independence.gauss_invariance_suffstat(obs_data,
                                                                                      iv_samples_list)
             alpha = 1e-3
             alpha_inv = 1e-3
             ci_tester = conditional_independence.MemoizedCI_Tester(conditional_independence.partial_correlation_test,
                                                                    obs_suffstat, alpha=alpha)
             invariance_tester = conditional_independence.MemoizedInvarianceTester(
-                conditional_independence.gauss_invariance_suffstat,
+                conditional_independence.gauss_invariance_test,
                 invariance_suffstat, alpha=alpha_inv)
 
             est_dag = igsp(setting_list, nodes, ci_tester, invariance_tester)
@@ -112,27 +113,30 @@ def run_random():
 
 
 def run_random_large():
-        data = pd.read_csv("./random_test_set_1000_{}/data_joint_{}.csv".format("small_world", 0))
-        obs_data = data.iloc[data['target'] == 0]
-        int_data = data.iloc[data['target'] != 0]
-        obs_data_no_targets = obs_data.loc[:, obs_data.columns != 'target'].to_numpy()
-        int_data_no_targets = int_data.loc[:, int_data.columns != 'target']
-        iv_samples_list = [np.expand_dims(row.to_numpy(), axis=0) for _, row in int_data_no_targets.iterrows()]
+        data = pd.read_csv("./random_test_set_1000_{}/data_joint_{}.csv".format("small_norm", 0))
+        obs_data = data.loc[data['target'] == 0]
+        int_data = data.loc[data['target'] != 0]
+        targets_list = set(list(int_data['target'].to_numpy()))
+
+        obs_data = obs_data.loc[:, obs_data.columns != 'target'].to_numpy()
+
+        iv_samples_list = [int_data.loc[int_data['target'] == t] for t in targets_list]
+        iv_samples_list = [inter.loc[:, inter.columns != 'target'] for inter in iv_samples_list]
 
         targets_list = set(list(int_data['target'].to_numpy()))
         nodes = targets_list
 
         setting_list = [dict(interventions=[t]) for t in targets_list]
 
-        obs_suffstat = conditional_independence.partial_correlation_suffstat(obs_data_no_targets)
-        invariance_suffstat = conditional_independence.gauss_invariance_suffstat(obs_data_no_targets,
+        obs_suffstat = conditional_independence.partial_correlation_suffstat(obs_data)
+        invariance_suffstat = conditional_independence.gauss_invariance_suffstat(obs_data,
                                                                                  iv_samples_list)
         alpha = 1e-3
         alpha_inv = 1e-3
         ci_tester = conditional_independence.MemoizedCI_Tester(conditional_independence.partial_correlation_test,
                                                                obs_suffstat, alpha=alpha)
         invariance_tester = conditional_independence.MemoizedInvarianceTester(
-            conditional_independence.gauss_invariance_suffstat,
+            conditional_independence.gauss_invariance_test,
             invariance_suffstat, alpha=alpha_inv)
 
         est_dag = igsp(setting_list, nodes, ci_tester, invariance_tester)
@@ -143,5 +147,5 @@ def run_random_large():
 
 run_regulon_db()
 # run_dream4()
-# run_random()
+#run_random()
 run_random_large()

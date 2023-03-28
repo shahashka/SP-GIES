8e40fa1e078f4d704a45ae2db8c8b46febcccb44
Ashka Shah <shahashka11@gmail.com>
2023-03-28 11:21:31 -0500

    naive MB based implementation

--- compare_algs.py
+++ compare_algs.py
@@ -2,12 +2,11 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import networkx as nx
-from sklearn.metrics import auc
 import itertools
 import warnings
 import time
-import cdt
-from cdt.causality.graph import GIES, PC
+
+from utils import get_scores, edge_to_dag, edge_to_adj, adj_to_dag
 warnings.filterwarnings("ignore")
 font = {'family' : 'normal',
         'weight' : 'bold',
@@ -17,62 +16,6 @@ matplotlib.rc('font', **font)
 
 # Script to compare PC, GES-O (observational only), GIES-IO (observational + interventional joint), and SP-GIES-IO algorhtms
 # on random, dream4 and regulondb datasets
-
-# Helper function to convert an adjacency matrix into a Networkx Digraph
-def adj_to_dag(adj, all_nodes,fixed_edges=None):
-    dag = nx.DiGraph()
-    dag.add_nodes_from(all_nodes)
-    for i in range(adj.shape[0]):
-        for j in range(adj.shape[1]):
-            if np.abs(adj[i,j]) > 0:
-                dag.add_edge(all_nodes[i], all_nodes[j], weight=np.abs(adj[i,j]))
-    if fixed_edges:
-        dag.add_edges_from(fixed_edges)
-    return dag
-
-# Helper function to convert a list of edges into an adjacency matrix
-def edge_to_adj(edges, all_nodes):
-    adj_mat = np.zeros((len(all_nodes), len(all_nodes)))
-    for e in edges:
-        start = all_nodes.index(e[0])
-        end = all_nodes.index(e[1])
-        adj_mat[start,end] = 1
-    return adj_mat
-
-# Helper function to convert a list of edges into a Networkx Digraph
-def edge_to_dag(edges):
-    dag = nx.DiGraph()
-    dag.add_edges_from(edges)
-    return dag
-
-# Helper function to print the SHD, SID, AUC for a set of algorithms and networks
-# Also handles averaging over several sets of networks (e.g the random comparison averages over 30 different generated graphs)
-# Default turn off sid, since it is computationally expensive
-def get_scores(alg_names, networks, ground_truth, get_sid=False):
-    for name, net in zip(alg_names, networks):
-        if type(net) == list and type(ground_truth) == list:
-            shd = 0
-            sid = 0
-            auc = 0
-            for n,g in zip(net, ground_truth):
-                shd += cdt.metrics.SHD(g, n, False)
-                sid += cdt.metrics.SID(g, n) if get_sid else 0
-                auc +=  cdt.metrics.precision_recall(g, n)[0]
-            print("{} {} {} {}".format(name, shd/len(net), sid/len(net), auc/len(net)))
-        elif type(net) != list and type(ground_truth) == list:
-            shd = 0
-            sid = 0
-            auc = 0
-            for g in ground_truth:
-                shd += cdt.metrics.SHD(g, net, False)
-                sid +=cdt.metrics.SID(g, net) if get_sid else 0
-                auc +=  cdt.metrics.precision_recall(g, net)[0]
-            print("{} {} {} {}".format(name, shd/len(ground_truth), sid/len(ground_truth), auc/len(ground_truth)))
-        else:
-            shd = cdt.metrics.SHD(ground_truth, net, False)
-            sid = cdt.metrics.SID(ground_truth, net) if get_sid else 0
-            auc, pr = cdt.metrics.precision_recall(ground_truth, net)
-            print("{} {} {} {}".format(name, shd, sid, auc))
 
 
 # Evaluate the performance of algorithms on the regulondb dataset
--- sp_gies_parallel.py
+++ sp_gies_parallel.py
@@ -5,55 +5,161 @@ from sklearn.cluster import SpectralClustering
 import networkx as nx
 import functools
 from concurrent.futures import ProcessPoolExecutor
-
+import os
+import rpy2.robjects as robjects
+from rpy2.robjects.packages import importr
+import rpy2.robjects as ro
+import rpy2.robjects.numpy2ri
+from utils import edge_to_dag, adj_to_edge, adj_to_dag, get_scores
+rpy2.robjects.numpy2ri.activate()
+pcalg = importr('pcalg')
+base = importr('base')
 # PARALLEL STRUCTURE LEARNING
-# PSEUDO CODE
-# Use cuPC to estimate the skeleton of the graph
-# Find MB(.)(Markov blanket for each vertex int the graph) with distance=2 for all nodes
-# Launch parallel threads/processes in python. each thread is assigned to a chunk of MBs
-# In each thread run GIES on a subset of the data corresponding to the markov blanket, use the skeleton as fixedGap (also try no skeleton  to compare)
-# Collect all the graphs (on order of number of vertices). report any conflicts
+def sp_gies_py(data, skel, target_map, outdir):
+    fixed_gaps = np.array((skel == 0), dtype=int)
+    target_index = data.loc[:, 'target'].to_numpy()
+    target_index = np.array([0 if i == 0 else target_map[i] + 1 for i in target_index])
+    targets = np.unique(target_index)[1:]  # Remove 0 the observational target
+    target_index += 1  # R indexes from 1
+    data = data.drop(columns=['target']).to_numpy()
 
-def func(mb,  data, skeleton=A):
-    # Use MB to partition data and skeleotn
-    # Run GIES with input skeleton (or not)
-    return dag
+    nr, nc = data.shape
+    D = ro.r.matrix(data, nrow=nr, ncol=nc)
+    ro.r.assign("data", D)
 
+    rcode = ','.join(str(int(i)) for i in targets)
+    rcode = 'append(list(integer(0)), list({}))'.format(rcode)
+    T = ro.r(rcode)
+    ro.r.assign("targets", T)
 
-def parallel_structure_learn(data):
-    # Use cuPC to estimate skeleton from data
+    TI = ro.IntVector(target_index)
+    ro.r.assign("target_index", TI)
 
-    # Get MB(.)
-    mrf = nx.from_numpy_array(A)
+    nr, nc = fixed_gaps.shape
+    FG = ro.r.matrix(fixed_gaps, nrow=nr, ncol=nc)
+    ro.r.assign("fixed_gaps", FG)
+
+    score = ro.r.new('GaussL0penIntScore', ro.r['data'], ro.r['targets'], ro.r['target_index'])
+    ro.r.assign("score", score)
+    result = pcalg.gies(ro.r['score'], fixedGaps=ro.r['fixed_gaps'], targets=ro.r['targets'], maxDegree=10)
+    ro.r.assign("result", result)
+    rcode = 'write.csv(result$repr$weight.mat(), row.names = FALSE,' \
+            ' file = paste("{}", "sp-gies-adj_mat.csv", sep=""))'.format(outdir)
+    ro.r(rcode)
+    return
+
+def markov_blankets(skeleton, data, outdir):
+    mrf = nx.from_numpy_array(skeleton)
     MB_X = []
-    nthreads = 8
-    nodes = list(data.columns)
-    nodes.remove('target')
+    data_local = []
+    maps = []
     for n in mrf.nodes():
         subgraph = nx.ego_graph(mrf, n, radius=2)
         MB_X.append(subgraph)
-    func_partial = functools.partial(
-        func,
-        data = data,
-        skeleton = A
-    )
-    results = []
-    chunksize = max(1, len(MB_X) // nthreads)
-    print("Launching processes")
-    with ProcessPoolExecutor(max_workers=nthreads) as executor:
-        for result in executor.map(func_partial, MB_X, chunksize=chunksize):
-            results.append(result)
 
-    G = nx.DiGraph()
-    G.add_nodes_from(nodes)
-    for dag in results:
-        G.add_edges_from(dag.edges())
-    # Print number of bidirectional edges
+        # Grab the subset of the data that corresponds to the MB
+        columns = list(subgraph.nodes)
+        # filter rows for interventional samples that are in subgraph and are observational
+        data_sub = data.loc[data['target'].isin(list(np.array(columns) + 1) + [0])]
+
+        target_map = pd.DataFrame({"GENE_ID": list(np.array(columns) + 1),
+                                   "COLUMN_INDEX": np.arange(len(columns))})
+        maps.append(target_map)
+
+        columns.append(-1)  # include the target column
+        data_sub = data_sub.iloc[:, columns]
+        data_local.append(data_sub)
+    local_structs = zip(MB_X, data_local, maps)
+    return local_structs
+
+def local_structure_learn(it, outdir):
+    # Run a structure learning algorithm on a subset of the data
+    # algorithms in include GIES, IGSP, etc..
+    print(it)
+    data = pd.read_csv("{}/part_{}/data.csv".format(outdir, it), header=0)
+    skel = pd.read_csv("{}/part_{}/skel.csv".format(outdir, it), header=None).to_numpy()
+    target_map = pd.read_csv("{}/part_{}/map.csv".format(outdir, it), header=0)
+    target_map = dict(zip(target_map.iloc[:, 0], target_map.iloc[:, 1]))
+    sp_gies_py(data, skel, target_map,"{}/part_{}/".format(outdir, it) )
+    return 0
+
+def partition(skeleton, data, outdir):
+    # Partition the skeleton according to some notion of locality
+    # Save the sub-skeleton and sub-data in different folders
+    local_structs = markov_blankets(skeleton, data, outdir)
+    for i,(s, d, m) in enumerate(local_structs):
+        if not os.path.exists("{}/part_{}/".format(outdir,i)):
+            os.makedirs("{}/part_{}/".format(outdir,i))
+        adj_mat = nx.adjacency_matrix(s)
+        np.savetxt("{}/part_{}/skel.csv".format(outdir,i), adj_mat.toarray(), delimiter=",")
+        d.to_csv("{}/part_{}/data.csv".format(outdir,i), index=False)
+        m.to_csv("{}/part_{}/map.csv".format(outdir, i), index=False)
+    return 0
 
 
-#edges = pd.read_csv("./random_test_set_1000_small_norm/bn_network_0.csv")
-#
-# # convert edges to the true graph
-# edges_pos = [(r['start'], r['end']) for i, r in edges.iterrows() if r['edge'] == 1]
-# nodes = ["G{}".format(i) for i in range(1,10001)]
-# A = edge_to_und_adj(edges_pos, nodes)
+def skeleton(data, outdir):
+    # Generate a skeleton of the causal graph based on data
+    # Methods include PC, GENIE, etc...
+    # Save adjacency matrix is outdir
+
+    # Read cuPC generated skeleton
+    return pd.read_csv("./random_test_set_100_small/0_cupc-adj_mat.csv", header=0).to_numpy()
+
+def resolve_global(outdir, num_partitions):
+    # Read all dags in directory and create a global dag
+    # Report number of conflicts
+    edge_list = []
+    for i in range(num_partitions):
+        adj_mat = pd.read_csv("{}/part_{}/sp-gies-adj_mat.csv".format(outdir, i), header=0).to_numpy()
+        map = pd.read_csv("{}/part_{}/map.csv".format(outdir, i), header=0)
+        nodes = map.iloc[:,0]
+        nodes = ["G{}".format(i) for i in nodes]
+        edge_list += adj_to_edge(adj_mat, nodes)
+    G = nx.DiGraph(edge_list)
+    print(0.5 * len( [ 1 for (u,v) in G.edges() if u in G[v] ] ))
+    return G
+
+def global_structure_learn(data, outdir):
+    partitioned = True
+    locally_resolved = True
+
+    if not partitioned:
+        global_skel = skeleton(data, outdir)
+        partition(global_skel, data, outdir)
+
+    num_partitions = len(os.listdir(outdir))
+    if not locally_resolved:
+        nthreads = 2
+        func_partial = functools.partial(
+            local_structure_learn,
+            outdir=outdir
+        )
+        results = []
+        chunksize = max(1, num_partitions // nthreads)
+        print("Launching processes")
+        with ProcessPoolExecutor(max_workers=nthreads) as executor:
+            for result in executor.map(func_partial, np.arange(num_partitions), chunksize=chunksize):
+                results.append(result)
+    G = resolve_global(outdir, num_partitions)
+    return G
+
+
+
+if __name__ == '__main__':
+    data = pd.read_csv("./random_test_set_100_small/data_joint_0.csv", header=0)
+    sp_gies_network = pd.read_csv("./random_test_set_100_small/0_sp-gies-adj_mat.csv", header=0).to_numpy()
+    nodes = list(data.columns)
+    nodes.remove('target')
+    sp_gies_graph = adj_to_dag(sp_gies_network, nodes)
+
+    edges = pd.read_csv("./random_test_set_100_small/bn_network_0.csv", header=0)
+    edges_pos = [(r['start'], r['end']) for i, r in edges.iterrows() if r['edge'] == 1]
+    G_true = edge_to_dag(edges_pos)
+
+    outdir =  "./random_test_set_100_small/parallel_test"
+    if not os.path.exists(outdir):
+        os.makedirs(outdir)
+    G_est = global_structure_learn(data, outdir)
+    print(G_true, G_est)
+    print(get_scores(["PARALLEL-SP-GIES", "SP-GIES"], [G_est, sp_gies_graph], G_true, get_sid=True))
+
